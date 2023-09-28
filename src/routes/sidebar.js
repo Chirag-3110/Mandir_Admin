3a4bd21fe501711f84dab5d1194ef66c1e607363
Chirag <takchirag828@gmail.com>
2023-09-28 23:31:39 +0530

    ready fro first build

--- src/routes/sidebar.js
+++ src/routes/sidebar.js
@@ -91,18 +91,18 @@ const routes = [
   //     },
   //   ]
   // },
-  {
-    path: '', //no url needed as this has submenu
-    icon: <Cog6ToothIcon className={`${iconClasses} inline` }/>, // icon component
-    name: 'Settings', // name that appear in Sidebar
-    submenu : [
-      {
-        path: '/settings-profile', //url
-        icon: <UserIcon className={submenuIconClasses}/>, // icon component
-        name: 'Profile', // name that appear in Sidebar
-      },
-    ]
-  },
+  // {
+  //   path: '', //no url needed as this has submenu
+  //   icon: <Cog6ToothIcon className={`${iconClasses} inline` }/>, // icon component
+  //   name: 'Settings', // name that appear in Sidebar
+  //   submenu : [
+  //     {
+  //       path: '/settings-profile', //url
+  //       icon: <UserIcon className={submenuIconClasses}/>, // icon component
+  //       name: 'Profile', // name that appear in Sidebar
+  //     },
+  //   ]
+  // },
   
 ]
 
