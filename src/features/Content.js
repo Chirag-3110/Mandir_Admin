1dabe73f01983a2da45b512216e3069c56f87152
Rohit Saini <rohitpsaini005@gmail.com>
2023-10-17 23:53:32 +0530

    ads

--- src/features/Content.js
+++ src/features/Content.js
@@ -14,17 +14,17 @@ const Content = () => {
                     <button className="btn px-6 btn-sm normal-case btn-primary" >Save</button>
                 </div>
             </>}>
-                <select value={selected} onSelect={(value) => {
-                    setSelected(value)
+                <select value={selected} onChange={(value) => {
+                    setSelected(value.target.value)
                 }}>
                     <option value="Terms And Condition">Terms And Condition</option>
                     <option value="Privacy Policy">Privacy Policy</option>
                     <option value="About Us">About Us</option>
                 </select>
-                <ReactQuill value={selected == "Terms And Condition" ? terms : selected == "Privacy Policy" ? Privacu : abour} onChange={(e)=>{
+                {
+                    selected == "Terms And Condition" ? <ReactQuill defaultValue={terms} value={terms} onChange={(e)=>{setTerms(e)}} style={{ marginTop: 20, height: "50vh", marginBottom: 20 }} /> : selected == "Privacy Policy" ? <ReactQuill defaultValue={Privacu} value={Privacu} onChange={(e)=>{setPrivacu(e)}} style={{ marginTop: 20, height: "50vh", marginBottom: 20 }} /> : <ReactQuill defaultValue={abour} value={abour} onChange={(e)=>{setAbout(e)}} style={{ marginTop: 20, height: "50vh", marginBottom: 20 }} />
 
-                   
-                }} style={{ marginTop: 20, height: "50vh", marginBottom: 20 }} />
+                }
 
             </TitleCard>
 
